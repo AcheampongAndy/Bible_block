a672fb1d9c5357667f0bfe88a6d49e6905149125
AcheampongAndy <acheampongandrews1999@gmail.com>
2024-03-11 23:24:37 +0000

    done

--- flask_block/__init__.py
+++ flask_block/__init__.py
@@ -10,7 +10,7 @@ from flask_mail import Mail
 app = Flask(__name__)
 
 # Configure the secret key for the Flask application
-app.config["SECRET_KEY"] = 'b7736b7af313ca79ca4a62ba539f1871'
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
 # Configure the URI for the SQLite database using the app's root path
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(os.path.dirname(app.root_path), 'flask_block', 'site.db')
@@ -30,8 +30,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'noreplydemo907@gmail.com'
-app.config['MAIL_PASSWORD'] = 'nbqa qeyx rrzx sioy'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
 # Create a Mail instance for sending emails
 mail = Mail(app)
